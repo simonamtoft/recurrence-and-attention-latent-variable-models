1ee2cd8d508a9b13db908d42ddb75c7a905f514a
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-03-13 16:38:34 +0100

    add DeterministicWarmup

---
+++ utils/training_utils.py
@@ -0,0 +1,19 @@
+import torch
+from torch.autograd import Variable
+
+
+class DeterministicWarmup(object):
+    """ Linear deterministic warm-up """
+    def __init__(self, n=100, t_max=1):
+        self.t = 0
+        self.t_max = t_max
+        self.inc = 1/n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        t = self.t + self.inc
+
+        self.t = self.t_max if t > self.t_max else t
+        return self.t
